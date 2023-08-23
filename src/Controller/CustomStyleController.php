fd5f3d74464697c28757f8938e7d92cb206973e0
asdfzdfj <nonemchome@hotmail.com>
2023-08-23 12:31:21 +0700

    generalize into custom style controller, changed paths

--- src/Controller/CustomStyleController.php
+++ src/Controller/CustomStyleController.php
@@ -2,24 +2,27 @@
 
 declare(strict_types=1);
 
-namespace App\Controller\Magazine;
+namespace App\Controller;
 
 use App\Controller\AbstractController;
 use App\Entity\Magazine;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class MagazineStyleController extends AbstractController
+class CustomStyleController extends AbstractController
 {
-    public function __invoke(Magazine $magazine, Request $request): Response
+    public function magazineCustomStyle(Magazine $magazine, Request $request): Response
     {
         $response = new Response();
         $response->headers->set('Content-Type', 'text/css');
         $response->setPublic();
-        $response->setMaxAge(3600);
+        $response->setMaxAge(60);
+        $response->headers->addCacheControlDirective('must-revalidate', true);
 
         if ($magazine->customCss) {
             $response->setContent($magazine->customCss);
+            $response->setEtag(md5($response->getContent()));
+            $response->isNotModified($request);
         } else {
             $response->setStatusCode(Response::HTTP_NOT_FOUND);
             $response->headers->set('Status', '404 Not Found');

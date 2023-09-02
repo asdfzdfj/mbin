b6934e7e60e6458b04ebbb7e47255599cb4c3227
asdfzdfj <nonemchome@hotmail.com>
2023-09-03 00:13:24 +0700

    add custom style endpoint for current user

--- src/Controller/CustomStyleController.php
+++ src/Controller/CustomStyleController.php
@@ -4,23 +4,41 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Controller\AbstractController;
 use App\Entity\Magazine;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class CustomStyleController extends AbstractController
 {
     public function magazineCustomStyle(Magazine $magazine, Request $request): Response
     {
+        $response = $this->createResponse($request, $magazine->customCss);
+        $response->setPublic();
+
+        return $response;
+    }
+
+    #[IsGranted('ROLE_USER')]
+    public function userCustomStyle(Request $request): Response
+    {
+        $user = $this->getUser();
+
+        $response = $this->createResponse($request, $user->customCss);
+        $response->setPrivate();
+
+        return $response;
+    }
+
+    private function createResponse(Request $request, ?string $customCss): Response
+    {
         $response = new Response();
         $response->headers->set('Content-Type', 'text/css');
-        $response->setPublic();
         $response->setMaxAge(60);
         $response->headers->addCacheControlDirective('must-revalidate', true);
 
-        if ($magazine->customCss) {
-            $response->setContent($magazine->customCss);
+        if ($customCss) {
+            $response->setContent($customCss);
             $response->setEtag(md5($response->getContent()));
             $response->isNotModified($request);
         } else {

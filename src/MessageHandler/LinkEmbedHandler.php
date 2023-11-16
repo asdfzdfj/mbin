d2875eb076b2d9d427c43d6f26294cb3d633f5ca
asdfzdfj <nonemchome@hotmail.com>
2023-11-16 18:12:06 +0700

    add cache prefix for markdown cache key

--- src/MessageHandler/LinkEmbedHandler.php
+++ src/MessageHandler/LinkEmbedHandler.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\MessageHandler;
 
+use App\Entity\Embed;
+use App\Markdown\Event\BuildCacheContext;
+use App\Markdown\Event\ConvertMarkdown;
 use App\Message\LinkEmbedMessage;
 use App\Repository\EmbedRepository;
-use App\Utils\Embed;
+use App\Utils\Embed as EmbedFetcher;
 use Psr\Cache\CacheItemPoolInterface;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
@@ -15,8 +19,9 @@ class LinkEmbedHandler
 {
     public function __construct(
         private readonly EmbedRepository $embedRepository,
-        private readonly Embed $embed,
-        private readonly CacheItemPoolInterface $markdownCache
+        private readonly EmbedFetcher $embed,
+        private readonly CacheItemPoolInterface $markdownCache,
+        private readonly EventDispatcherInterface $dispatcher,
     ) {
     }
 
@@ -28,7 +33,7 @@ class LinkEmbedHandler
             try {
                 $embed = $this->embed->fetch($url)->html;
                 if ($embed) {
-                    $entity = new \App\Entity\Embed($url, true);
+                    $entity = new Embed($url, true);
                     $this->embedRepository->add($entity);
                 }
             } catch (\Exception $e) {
@@ -36,11 +41,13 @@ class LinkEmbedHandler
             }
 
             if (!$embed) {
-                $entity = new \App\Entity\Embed($url, false);
+                $entity = new Embed($url, false);
                 $this->embedRepository->add($entity);
             }
         }
 
-        $this->markdownCache->deleteItem(hash('sha256', json_encode(['content' => $message->body])));
+        $cacheContext = new BuildCacheContext(new ConvertMarkdown($message->body));
+        $this->dispatcher->dispatch($cacheContext);
+        $this->markdownCache->deleteItem($cacheContext->getCacheKey());
     }
 }

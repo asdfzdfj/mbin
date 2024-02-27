0888ad3712b95e9c58eb98f4f00f7ec3d02e3341
asdfzdfj <nonemchome@hotmail.com>
2024-02-27 17:20:13 +0700

    MONKEY: extremely scuffed way to sort replies to self first
    essentially an extremely bad bootleg ripoff of this changes from sharkey:
    https://activitypub.software/TransFem-org/Sharkey/-/merge_requests/424
    
    should be helpful when reading long threads, like those from foone,
    techconnectify, or ken shirriff
    
    only enabled for microblog posts for now

--- src/Twig/Runtime/SubjectExtensionRuntime.php
+++ src/Twig/Runtime/SubjectExtensionRuntime.php
@@ -4,17 +4,44 @@ declare(strict_types=1);
 
 namespace App\Twig\Runtime;
 
+use App\Entity\Entry;
+use App\Entity\EntryComment;
+use App\Entity\Post;
+use App\Entity\PostComment;
+use Psr\Log\LoggerInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
 class SubjectExtensionRuntime implements RuntimeExtensionInterface
 {
-    public function __construct()
-    {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {
         // Inject dependencies if needed
     }
 
-    public function doSomething($value)
-    {
-        // ...
+    /**
+     * @param iterable<Entry|Post|EntryComment|PostComment> $replies
+     */
+    public function sortAuthorFirst(
+        iterable $replies,
+        Entry|Post|EntryComment|PostComment $root,
+        bool $enabled = true
+    ): iterable {
+        if (!$enabled) {
+            return $replies;
+        }
+
+        $authorReplies = [];
+        $otherReplies = [];
+
+        foreach ($replies as $reply) {
+            if ($reply->user === $root->user) {
+                $authorReplies[] = $reply;
+            } else {
+                $otherReplies[] = $reply;
+            }
+        }
+
+        return array_merge($authorReplies, $otherReplies);
     }
 }

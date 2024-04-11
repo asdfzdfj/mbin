a6c36b68c67e192e33e5acde38043dc5bba7e4b5
asdfzdfj <nonemchome@hotmail.com>
2024-04-11 22:03:26 +0700

    make notification controller more bfcache friendly?
    - change mercure teardown to use pagehide instead of beforeunload
    - if mercure was connected before page being put into bfcache, it
      can now reconnect back to mercure on pageshow restored from bfcache
    - connect pageshow/hide event via controller actions rather than adding
      event listeners in controller `connect()`
    - pass mercure endpoint directly into the controller instead of having
      it dig the value out of the dom (essentially pulling them out of thin
      air)
      not passing a valid endpoint means it's disabled and would not connect
    
    even with this, the page may not get suspend into bfcache for firefox,
    it seems to have more strict requirements on what page could be put into
    bfcache, and having mercure connection _at all_ makes it not want to do
    that, and tearing down the connection on pagehide isn't good enough to
    make firefox considers caching them again
    
    all of this seems to be working fine with chrome's bfcache though

--- assets/controllers/notifications_controller.js
+++ assets/controllers/notifications_controller.js
@@ -1,9 +1,10 @@
-import {Controller} from '@hotwired/stimulus';
-import Subscribe from '../utils/event-source';
+import { buildTopics, subscribe } from '../utils/event-source';
+import { Controller } from '@hotwired/stimulus';
 
 /* stimulusFetch: 'lazy' */
 export default class extends Controller {
     static values = {
+        endpoint: String,
         user: String,
         magazine: String,
         entryId: String,
@@ -11,88 +12,75 @@ export default class extends Controller {
     };
 
     connect() {
-        this.es(this.getTopics());
-
-        window.onbeforeunload = function (event) {
-            if (window.es !== undefined) {
-                window.es.close();
-            }
-        };
+        if (this.endpointValue) {
+            this.connectEs();
+        }
     }
 
-    es(topics) {
-        if (window.es !== undefined) {
+    disconnect() {
+        this.terminateEs();
+    }
+
+    terminateEs() {
+        if (window.es instanceof EventSource) {
             window.es.close();
+            window.es = null;
         }
+    }
 
-        let self = this;
-        let cb = function (e) {
-            let data = JSON.parse(e.data);
+    suspendEs({ persisted }) {
+        this.terminateEs();
+        console.log('suspending es, terminating mercure connection', persisted);
+    }
 
-            self.dispatch(data.op, {detail: data});
-
-            self.dispatch('Notification', {detail: data});
-
-            // if (data.op.includes('Create')) {
-            //     self.dispatch('CreatedNotification', {detail: data});
-            // }
-
-            // if (data.op === 'EntryCreatedNotification' || data.op === 'PostCreatedNotification') {
-            //     self.dispatch('MainSubjectCreatedNotification', {detail: data});
-            // }
-            //
+    // only attempt restore when it's pulled out of bfcache
+    // and earlier connection has touched window.es var
+    restoreEs({ persisted }) {
+        if (persisted && undefined !== window.es) {
+            console.log('restoring es, reconnecting to mercure', persisted);
+            this.connectEs();
         }
+    }
 
-        const eventSource = Subscribe(topics, cb);
+    connectEs() {
+        this.terminateEs();
+
+        const endpoint = this.endpointValue;
+        const topics = buildTopics({
+            user: this.userValue,
+            magazine: this.magazineValie,
+            entryId: this.entryIdValue,
+            postId: this.postIdValue,
+        });
+        const callback = (e) => {
+            const data = JSON.parse(e.data);
+
+            this.dispatch(data.op, { detail: data });
+            this.dispatch('Notification', { detail: data });
+        };
+
+        const eventSource = subscribe(endpoint, topics, callback);
         if (eventSource) {
             window.es = eventSource;
+
             // firefox bug: https://bugzilla.mozilla.org/show_bug.cgi?id=1803431
-            if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
-                let resubscribe = (e) => {
-                    window.es.close();
-                    setTimeout(() => {
-                        const eventSource = Subscribe(topics, cb);
-                        if (eventSource) {
-                            window.es = eventSource;
-                            window.es.onerror = resubscribe;
-                        }
-                    }, 10000);
+            if (navigator.userAgent.toLowerCase().includes('firefox')) {
+                const resubscribe = () => {
+                    this.terminateEs();
+                    setTimeout(
+                        () => {
+                            const eventSource = subscribe(endpoint, topics, callback);
+                            if (eventSource) {
+                                window.es = eventSource;
+                                window.es.onerror = resubscribe;
+                            }
+                        },
+                        10000,
+                    );
                 };
+
                 window.es.onerror = resubscribe;
             }
         }
-    }
-
-    getTopics() {
-        let pub = true;
-        const topics = [
-            'count'
-        ]
-
-        if (this.userValue) {
-            topics.push(`/api/users/${this.userValue}`);
-            pub = true;
-        }
-
-        if (this.magazineValue) {
-            topics.push(`/api/magazines/${this.magazineValue}`);
-            pub = false;
-        }
-
-        if (this.entryIdValue) {
-            topics.push(`/api/entries/${this.entryIdValue}`);
-            pub = false;
-        }
-
-        if (this.postIdValue) {
-            topics.push(`/api/posts/${this.postIdValue}`);
-            pub = false;
-        }
-
-        if (pub) {
-            topics.push('pub');
-        }
-
-        return topics;
     }
 }
--- assets/utils/event-source.js
+++ assets/utils/event-source.js
@@ -1,17 +1,43 @@
-export default function
-    subscribe(topics, cb) {
-    const mercureElem = document.getElementById("mercure-url");
-    if (mercureElem) {
-        const url = new URL(mercureElem.textContent.trim());
-
-        topics.forEach(topic => {
-            url.searchParams.append('topic', topic);
-        })
-
-        const eventSource = new EventSource(url);
-        eventSource.onmessage = e => cb(e);
-
-        return eventSource;
+export function subscribe(endpoint, topics, callback) {
+    if (!endpoint) {
+        return null;
     }
-    return null;
+
+    const url = new URL(endpoint);
+
+    topics.forEach((topic) => {
+        url.searchParams.append('topic', topic);
+    });
+
+    const eventSource = new EventSource(url);
+    eventSource.onmessage = callback;
+
+    return eventSource;
+}
+
+export function buildTopics({ user, magazine, entryId, postId }) {
+    const topics = ['count'];
+    const pub = !(magazine || entryId || postId);
+
+    if (user) {
+        topics.push(`/api/users/${user}`);
+    }
+
+    if (magazine) {
+        topics.push(`/api/magazines/${magazine}`);
+    }
+
+    if (entryId) {
+        topics.push(`/api/entries/${entryId}`);
+    }
+
+    if (postId) {
+        topics.push(`/api/posts/${postId}`);
+    }
+
+    if (pub) {
+        topics.push('pub');
+    }
+
+    return topics;
 }
